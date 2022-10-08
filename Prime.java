941f4482068f8cfb7bfe51d94b8574533754f05f
PrernaKumari12 <91779020+PrernaKumari12@users.noreply.github.com>
2022-10-08 16:15:20 +0530

    Add files via upload

---
+++ Prime.java
@@ -0,0 +1,20 @@
+package basic;
+
+public class Prime {
+ public static void main(String[] args) {
+	int div=2;
+	int n=2;
+	boolean flag=true;
+	while(div<n) {
+		int divisor=div;
+		
+		if(n%div==0) {
+			flag=false;
+		}
+		
+		
+		
+	}
+	System.out.println(flag);
+}
+}

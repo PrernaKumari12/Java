30978b4c1d5f7d3910638f81d8384d267e6b1283
AnkitKumar989 <91775514+AnkitKumar989@users.noreply.github.com>
2022-10-08 16:26:03 +0530

    Update Temp.java
    loop condition was wrong

--- Temp.java
+++ Temp.java
@@ -6,7 +6,7 @@ public class Temp {
 		int fmax=300;
 		int fstep=20;
 		int fcurrent=0;
-		while(fcurrent<fmax) {
+		while(fcurrent<=fmax) {
 			int c=(int)((5.0/9)*(fcurrent-32));
 			System.out.println(fcurrent+"\t"+c);
 			fcurrent=fcurrent+fstep;

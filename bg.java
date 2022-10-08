d4ae37e4bea3d5bacd9db240c6d421ef93ed916f
PrernaKumari12 <91779020+PrernaKumari12@users.noreply.github.com>
2022-10-08 16:22:32 +0530

    Add files via upload

---
+++ bg.java
@@ -0,0 +1,29 @@
+package basic;
+
+import java.util.Scanner;
+
+public class bg {
+	static Scanner scn = new Scanner(System.in);
+
+	public static void main(String[] args) {
+		int n1 = scn.nextInt();
+		int n2 = scn.nextInt();
+		int sum = 0;
+		int i = 1;
+		int count = 0;
+		for (int row = 1; row <= n1; row++) {
+			while (count < n1) {
+
+				sum = 3 * i + 2;
+				if (sum % n2 != 0) {
+					System.out.println(sum );
+					count++;
+				}
+			
+			}
+
+		}
+
+	}
+
+}
